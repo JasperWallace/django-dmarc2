840d10069728837f423cf9dd40fb26cf7cb54f76
VirtualTam <virtualtam@flibidi.net>
2017-12-30 19:51:52 +0100

    Enable the dmarc app for the demo project
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- demo/settings.py
+++ demo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dmarc',
 )
 
 MIDDLEWARE_CLASSES = (
--- demo/urls.py
+++ demo/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from dmarc import urls as dmarc_urls
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^dmarc/', include(dmarc_urls, namespace='dmarc')),
 ]
