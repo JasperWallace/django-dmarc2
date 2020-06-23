650505246e246e2c3ecabc668cf409d2b2edaecc
Jasper Wallace <jasper@pointless.net>
2020-06-23 01:21:55 +0100

    Test zip files as well and use a gz email for the duplicate test

--- dmarc/test_import.py
+++ dmarc/test_import.py
@@ -111,6 +111,64 @@ class ImportDMARCReportTestCase(TestCase):
         self.assertEqual(data[1].domain, 'p-o.co.uk')
         self.assertEqual(data[1].result, 'pass')
 
+    def test_importdmarcreport_file_zip(self):
+        """Test importing an email with the xml zipped file"""
+        out = StringIO()
+        data = Reporter.objects.all()
+        self.assertEqual(len(data), 0)
+
+        dmarcreport = os.path.dirname(os.path.realpath(__file__))
+        dmarcreport = os.path.join(dmarcreport, 'tests/dmarcreport-email-zip.xml')
+        call_command('importdmarcreport', '--email', dmarcreport, stdout=out)
+        self.assertIn('', out.getvalue())
+
+        # Reporter object
+        data = Reporter.objects.all()
+
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0].org_name, 'Persistent Objects')
+        self.assertEqual(data[0].email, 'ahicks@p-o.co.uk')
+        # Report object
+        data = Report.objects.all()
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0].report_id, '5edbe461-ccda-1e41-abdb-00c0af3f9715@p-o.co.uk')
+        if settings.USE_TZ:
+            tz_utc = pytz.timezone(settings.TIME_ZONE)
+            self.assertEqual(data[0].date_begin, datetime(2015, 2, 25, 12, 0, tzinfo=tz_utc))
+            self.assertEqual(data[0].date_end, datetime(2015, 2, 26, 12, 0, tzinfo=tz_utc))
+        else:
+            self.assertEqual(data[0].date_begin, datetime(2015, 2, 25, 12, 0))
+            self.assertEqual(data[0].date_end, datetime(2015, 2, 26, 12, 0))
+        self.assertEqual(data[0].policy_domain, 'p-o.co.uk')
+        self.assertEqual(data[0].policy_adkim, 'r')
+        self.assertEqual(data[0].policy_aspf, 'r')
+        self.assertEqual(data[0].policy_p, 'quarantine')
+        self.assertEqual(data[0].policy_sp, 'none')
+        self.assertEqual(data[0].policy_pct, 100)
+        self.assertIn("<?xml version='1.0' encoding='utf-8'?>", data[0].report_xml)
+        self.assertIn("<feedback>", data[0].report_xml)
+        # Record
+        data = Record.objects.all()
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0].source_ip, ipaddress.ip_address('80.229.143.200'))
+        self.assertEqual(data[0].recordcount, 1)
+        self.assertEqual(data[0].policyevaluated_disposition, 'none')
+        self.assertEqual(data[0].policyevaluated_dkim, 'pass')
+        self.assertEqual(data[0].policyevaluated_spf, 'pass')
+        self.assertEqual(data[0].policyevaluated_reasontype, '')
+        self.assertEqual(data[0].policyevaluated_reasoncomment, '')
+        self.assertEqual(data[0].identifier_headerfrom, 'p-o.co.uk')
+
+        # Result
+        data = Result.objects.all()
+        self.assertEqual(len(data), 2)
+        self.assertEqual(data[0].record_type, 'spf')
+        self.assertEqual(data[0].domain, 'p-o.co.uk')
+        self.assertEqual(data[0].result, 'pass')
+        self.assertEqual(data[1].record_type, 'dkim')
+        self.assertEqual(data[1].domain, 'p-o.co.uk')
+        self.assertEqual(data[1].result, 'pass')
+
     def test_importdmarcreport_file_duplicate(self):
         """Test importing a duplicate xml file"""
         out = StringIO()
@@ -118,8 +176,8 @@ class ImportDMARCReportTestCase(TestCase):
         self.assertEqual(len(data), 0)
 
         dmarcreport = os.path.dirname(os.path.realpath(__file__))
-        dmarcreport = os.path.join(dmarcreport, 'tests/dmarcreport.xml')
-        call_command('importdmarcreport', '--xml', dmarcreport, stdout=out)
+        dmarcreport = os.path.join(dmarcreport, 'tests/dmarcreport-email-gz.xml')
+        call_command('importdmarcreport', '--email', dmarcreport, stdout=out)
         self.assertIn('', out.getvalue())
 
         # Reporter object
