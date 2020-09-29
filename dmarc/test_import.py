ff2d8a82915a50c7130d56f34abfd3ba48987e4d
Jasper Wallace <jasper@pointless.net>
2020-09-29 06:04:44 +0100

    Add a google report for testing purposes

--- dmarc/test_import.py
+++ dmarc/test_import.py
@@ -113,6 +113,61 @@ class ImportDMARCReportTestCase(TestCase):
         self.assertEqual(data[1].domain, 'p-o.co.uk')
         self.assertEqual(data[1].result, 'pass')
 
+    def test_importdmarcreport_file_google(self):
+        """Test importing an xml file from google"""
+        out = StringIO()
+        data = Reporter.objects.all()
+        self.assertEqual(len(data), 0)
+        dmarcreport = os.path.dirname(os.path.realpath(__file__))
+        dmarcreport = os.path.join(dmarcreport, 'tests/google.xml')
+        call_command('importdmarcreport', '--xml', dmarcreport, stdout=out)
+        self.assertIn('', out.getvalue())
+        # Reporter object
+        data = Reporter.objects.all()
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0].org_name, 'google.com')
+        self.assertEqual(data[0].email, 'noreply-dmarc-support@google.com')
+        # Report object
+        data = Report.objects.all()
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0].report_id, '123456789')
+        if settings.USE_TZ:
+            tz_utc = pytz.timezone(settings.TIME_ZONE)
+            self.assertEqual(data[0].date_begin, datetime(2015, 2, 25, 12, 0, tzinfo=tz_utc))
+            self.assertEqual(data[0].date_end, datetime(2015, 2, 26, 12, 0, tzinfo=tz_utc))
+        else:
+            self.assertEqual(data[0].date_begin, datetime(2015, 2, 25, 12, 0))
+            self.assertEqual(data[0].date_end, datetime(2015, 2, 26, 12, 0))
+        self.assertEqual(data[0].policy_domain, 'example.net')
+        self.assertEqual(data[0].policy_adkim, 'r')
+        self.assertEqual(data[0].policy_aspf, 'r')
+        self.assertEqual(data[0].policy_p, 'none')
+        self.assertEqual(data[0].policy_sp, 'none')
+        self.assertEqual(data[0].policy_pct, 100)
+        self.assertIn("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>", data[0].report_xml)
+        self.assertIn("<feedback>", data[0].report_xml)
+        # Record
+        data = Record.objects.all()
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0].source_ip, ipaddress.ip_address('2001:DB8::1'))
+        self.assertEqual(data[0].recordcount, 2)
+        self.assertEqual(data[0].policyevaluated_disposition, 'none')
+        self.assertEqual(data[0].policyevaluated_dkim, 'pass')
+        self.assertEqual(data[0].policyevaluated_spf, 'pass')
+        self.assertEqual(data[0].policyevaluated_reasontype, '')
+        self.assertEqual(data[0].policyevaluated_reasoncomment, '')
+        self.assertEqual(data[0].identifier_headerfrom, 'lists.example.net')
+
+        # Result
+        data = Result.objects.all()
+        self.assertEqual(len(data), 2)
+        self.assertEqual(data[0].record_type, 'dkim')
+        self.assertEqual(data[0].domain, 'example.net')
+        self.assertEqual(data[0].result, 'pass')
+        self.assertEqual(data[1].record_type, 'spf')
+        self.assertEqual(data[1].domain, 'lists.example.net')
+        self.assertEqual(data[1].result, 'pass')
+
     def test_importdmarcreport_file_zip(self):
         """Test importing an email with the xml zipped file"""
         out = StringIO()
